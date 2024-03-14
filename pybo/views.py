eae8c9328584fbd196f421c2d4d433cc8acccddb
yeowul <dldudnf0404@gmail.com>
2024-03-14 16:34:43 +0900

    first commit

---
+++ pybo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question,Answer
+from .forms import QuestionForm
+
+def index(request):
+    question_list=Question.objects.order_by('-create_date')
+    context={'question_list':question_list}
+    return render(request,'pybo/question_list.html',context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('pybo:detail',question_id=question.id)
+
+def question_create(request):
+    form = QuestionForm()
+    return render(request, 'pybo/question_form.html', {'form': form})
